8e9b7957ba6bc1b93c9845d8b747687c836585c8
stevenseb <stevenseb@hotmail.com>
2024-03-09 14:32:51 -0500

    camel case columns and add boilerplate for server

---
+++ backend/app.js
@@ -0,0 +1,61 @@
+// Imports
+
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+const routes = require('./routes');
+
+
+
+
+app.use(routes); // Connect all the routes
+const app = express();
+// Express using json
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+
+// Middleware
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+  
+  // helmet helps set a variety of headers to better secure your app
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+  
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+// Root route - DO NOT MODIFY
+app.get('/', (req, res) => {
+    res.json({
+        message: "API server is running"
+    });
+});
+
+// Set port and listen for incoming requests - DO NOT MODIFY
+if (require.main === module) {
+    const port = 8000;
+    app.listen(port, () => console.log('Server is listening on port', port));
+} else {
+    module.exports = app;
+}
