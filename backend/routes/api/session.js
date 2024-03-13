71f732d8df097d5087aa64ea579738304ec7f443
stevenseb <stevenseb@hotmail.com>
2024-03-13 15:38:33 -0400

    added firstName and lastName to user and tested user, session routes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -46,6 +46,8 @@ router.post(
 
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
       username: user.username,
     };
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -32,12 +32,14 @@ router.post(
   '/',
   validateSignup,
   async (req, res) => {
-    const { email, password, username } = req.body;
+    const { firstName, lastName, email, password, username } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ email, username, hashedPassword });
+    const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
       username: user.username,
     };
@@ -50,28 +52,6 @@ router.post(
   }
 );
 
-
-// SIGN UP USER
-router.post(
-    '/',
-    async (req, res) => {
-      const { email, password, username } = req.body;
-      const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
-  
-      const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-      };
-  
-      await setTokenCookie(res, safeUser);
-  
-      return res.json({
-        user: safeUser
-      });
-    }
-  );
 
 
 module.exports = router;
