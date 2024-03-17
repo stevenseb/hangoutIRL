9baceecf36fad3fca6fec9b402d4a0a84b702306
stevenseb <stevenseb@hotmail.com>
2024-03-17 11:45:51 -0400

    debug

--- backend/db/migrations/20240314235349-create-event.js
+++ backend/db/migrations/20240314235349-create-event.js
@@ -20,18 +20,18 @@ module.exports = {
       groupId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: {
-          model: 'Groups',
-          key: 'id',
-        },
+        // references: {
+        //   model: 'Groups',
+        //   key: 'id',
+        // },
       },
       venueId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        references: {
-          model: 'Venues',
-          key: 'id',
-        },
+        // references: {
+        //   model: 'Venues',
+        //   key: 'id',
+        // },
       },
       name: {
         type: Sequelize.STRING,
