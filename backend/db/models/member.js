4b6998b158bc4a9d88150b80369e420a02ce4e19
stevenseb <stevenseb@hotmail.com>
2024-03-17 12:28:17 -0400

    debug

--- backend/db/models/member.js
+++ backend/db/models/member.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Member.hasMany(models.User, { foreignKey: 'userId' });
-      Member.hasMany(models.Group, { foreignKey: 'groupId' });
+      //Member.hasMany(models.User, { foreignKey: 'userId' });
+      //Member.hasMany(models.Group, { foreignKey: 'groupId' });
     }
   }
   Member.init({
