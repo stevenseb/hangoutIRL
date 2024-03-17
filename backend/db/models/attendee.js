ba31a19882a0f6f7f9a678212ce167a8928e8ab1
stevenseb <stevenseb@hotmail.com>
2024-03-17 11:59:27 -0400

    debug associations

--- backend/db/models/attendee.js
+++ backend/db/models/attendee.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Attendee.hasMany(models.User, { foreignKey: 'userId' });
+      //Attendee.hasMany(models.User, { foreignKey: 'userId' });
       //Attendee.hasMany(models.Event, { foreignKey: 'eventId'});
     }
   }
